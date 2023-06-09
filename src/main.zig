650e42e4ade43cfb411129417d4ae4730663e7c3
EugenHotaj <eugen_hotaj_91@hotmail.com>
2023-06-08 22:14:54 -0400

    Remove test code in main.

--- src/main.zig
+++ src/main.zig
@@ -53,24 +53,3 @@ pub fn load_tensor(path: []const u8, shape: []const usize, allocator: *const std
     _ = try fd.readAll(tensor);
     return std.mem.bytesAsSlice(f32, @alignCast(@alignOf(f32), tensor));
 }
-
-pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-
-    const w_path = "models/124M/raw/model-h0-mlp-c_fc-w";
-    const w_shape = [_]usize{ 3072, 768 };
-    const w_tensor = try load_tensor(w_path, &w_shape, &allocator);
-
-    const b_path = "models/124M/raw/model-h0-mlp-c_fc-w";
-    const b_shape = [_]usize{3072};
-    const b_tensor = try load_tensor(b_path, &b_shape, &allocator);
-
-    const layer = Linear(768, 3072).init(w_tensor, b_tensor);
-
-    var inputs = try allocator.alloc(f32, 5 * 768);
-    var rng = std.rand.DefaultPrng.init(0);
-    for (0..5 * 768) |i| {
-        inputs[i] = rng.random().floatNorm(f32);
-    }
-    _ = try layer.forward(inputs, &allocator);
-}
