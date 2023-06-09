f7a3d42d07344b18fb0b248cb1250408869e1be9
EugenHotaj <eugen_hotaj_91@hotmail.com>
2023-06-08 20:48:46 -0400

    Minor changes.

--- download_weights.py
+++ download_weights.py
@@ -41,13 +41,16 @@ weights_dir = f"{model}/raw"
 if not os.path.exists(weights_dir):
     os.makedirs(weights_dir)
     checkpoint = tf.train.load_checkpoint(model)
-    variables = sorted(list(checkpoint.get_variable_to_shape_map().keys()))[:10]
+    variables = sorted(list(checkpoint.get_variable_to_shape_map().keys()))
     with tqdm(
         ncols=100, desc=f"Dumping raw weights", total=len(variables), unit_scale=True
     ) as pbar:
         for name in variables:
-            tensor = checkpoint.get_tensor(name).astype(np.float32).reshape(-1)
+            tensor = checkpoint.get_tensor(name).astype(np.float32).squeeze()
+            # Store weight tensors in column major format.
+            if name.endswith("/w"):
+                tensor = tensor.T
             fname = name.replace("/", "-")
             with open(f"{weights_dir}/{fname}", "wb") as file_:
-                file_.write(tensor.tobytes())
+                file_.write(tensor.reshape(-1).tobytes())
             pbar.update(1)
--- src/main.zig
+++ src/main.zig
@@ -5,15 +5,14 @@ pub fn Linear(comptime I: usize, comptime O: usize) type {
         const Self = @This();
         in_features: usize = I,
         out_features: usize = O,
-        weight: [I * O]f32, // Weights must be provided in *column major* order!
-        bias: [O]f32,
+        weight: []const f32, // Weights must be provided in *column major* order!
+        bias: []const f32,
 
-        pub fn init(weight: *[I * O]f32, bias: *[O]f32) Self {
-            // TODO(eugenhotaj): We're unnecessarily copying memory here.
-            return Self{ .weight = weight.*, .bias = bias.* };
+        pub fn init(weight: []const f32, bias: []const f32) Self {
+            return Self{ .weight = weight, .bias = bias };
         }
 
-        pub fn forward(self: Self, inputs: []f32, allocator: *std.mem.Allocator) !*[]f32 {
+        pub fn forward(self: Self, inputs: []f32, allocator: *const std.mem.Allocator) ![]f32 {
             const batch_size = inputs.len / I;
             var outputs = try allocator.alloc(f32, batch_size * self.out_features);
             for (0..batch_size) |b| {
@@ -25,7 +24,7 @@ pub fn Linear(comptime I: usize, comptime O: usize) type {
                     outputs[b * O + o] = sum + self.bias[o];
                 }
             }
-            return &outputs;
+            return outputs;
         }
     };
 }
@@ -46,22 +45,21 @@ pub fn load_tensor(path: []const u8, shape: []const usize, allocator: *const std
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
+
     const w_path = "models/124M/raw/model-h0-mlp-c_fc-w";
-    const shape = [_]usize{ 786, 3072 };
-    const tensor = try load_tensor(w_path, &shape, &allocator);
-    std.debug.print("{any}\n", .{tensor[0..10].*});
-    std.debug.print("{any}\n", .{tensor[2359286..2359296].*});
+    const w_shape = [_]usize{ 3072, 768 };
+    const w_tensor = try load_tensor(w_path, &w_shape, &allocator);
+
+    const b_path = "models/124M/raw/model-h0-mlp-c_fc-w";
+    const b_shape = [_]usize{3072};
+    const b_tensor = try load_tensor(b_path, &b_shape, &allocator);
+
+    const layer = Linear(768, 3072).init(w_tensor, b_tensor);
+
+    var inputs = try allocator.alloc(f32, 5 * 768);
+    var rng = std.rand.DefaultPrng.init(0);
+    for (0..5 * 768) |i| {
+        inputs[i] = rng.random().floatNorm(f32);
+    }
+    _ = try layer.forward(inputs, &allocator);
 }
-
-// pub fn main() !void {
-//     var allocator = std.heap.page_allocator;
-
-//     var weight = [_]f32{ -0.5384, 0.3873, 0.0247, -0.0153, 0.0466, 0.0597, -0.3315, -0.5699, -0.3644 };
-//     var bias = [_]f32{ 0.0489, -0.0189, 0.0355 };
-//     const layer = Linear(3, 3).init(&weight, &bias);
-
-//     var inputs = [_]f32{ 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0 };
-
-//     const outputs = try layer.forward(&inputs, &allocator);
-//     std.debug.print("{any}\n", .{outputs.*});
-// }
