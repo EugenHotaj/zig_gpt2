fe7ee8d77232cf81931cd5810c116967b296fbf0
EugenHotaj <eugen_hotaj_91@hotmail.com>
2023-06-19 01:11:02 -0400

    Add Tranformer Block.

--- generate_nano_gpt.py
+++ generate_nano_gpt.py
@@ -1,6 +1,7 @@
-"""Definition of the GPT Language Model largely copied from NanoGPT [1].
+"""Definition of GPT-2, largely copied from NanoGPT [1].
 
-Some things have been removed or simplified since they're not necessary for inference.
+NOTE: There is some divergence from NanoGPT, such as always including biases (since 
+GPT-2) uses them, using the default LayerNorm, not supporting flash attention, etc.
 
 [1]: https://github.com/karpathy/nanoGPT/blob/master/model.py
 """
@@ -17,10 +18,10 @@ from torch.nn import functional as F
 
 @dataclass
 class GPTConfig:
+    block_size: int = 1024
+    n_head: int = 12
     n_embd: int = 768
     dropout: float = 0.0
-    # Bias in Linears and LayerNorms, like GPT-2.
-    bias: bool = True
 
 
 def new_gelu(x):
@@ -35,10 +36,10 @@ def new_gelu(x):
 
 
 class MLP(nn.Module):
-    def __init__(self, config):
+    def __init__(self, config: GPTConfig):
         super().__init__()
-        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
         self.dropout = nn.Dropout(config.dropout)
 
     def forward(self, x):
@@ -49,33 +50,112 @@ class MLP(nn.Module):
         return x
 
 
-gpt_config = GPTConfig()
-mlp = MLP(gpt_config).eval()
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # Key, query, value projections for all heads, but in a batch.
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # Output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # Regularization.
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.dropout = config.dropout
+        # Causal mask.
+        bias = torch.tril(torch.ones(config.block_size, config.block_size))
+        bias = bias.view(1, 1, config.block_size, config.block_size)
+        self.register_buffer("bias", bias)
 
-n_embd = gpt_config.n_embd
+    def forward(self, x):
+        # Batch size, sequence length, embedding dimensionality (n_embd).
+        B, T, C = x.size()
+        hs = C // self.n_head
+
+        # Calculate query, key, values for all heads in batch and move head forward to
+        # be the batch dim.
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, hs).transpose(1, 2)  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, hs).transpose(1, 2)  # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, hs).transpose(1, 2)  # (B, nh, T, hs)
+
+        # Manual implementation of attention.
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
+        att = F.softmax(att, dim=-1)
+        att = self.attn_dropout(att)
+        y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+
+        # Re-assemble all head outputs side by side.
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+
+        # Output projection.
+        y = self.resid_dropout(self.c_proj(y))
+        return y
 
 
-with open("models/124M/raw/model-h0-mlp-c_fc-w", "rb") as file_:
+class Block(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+def load_linear(module: nn.Linear, layer_name: str, in_f: int, out_f: int) -> None:
+    with open(f"models/124M/raw/model-{layer_name}-w", "rb") as file_:
+        tensor = np.frombuffer(file_.read(), dtype=np.float32)
+        module.weight.data = torch.tensor(tensor).reshape(out_f, in_f)
+
+    with open(f"models/124M/raw/model-{layer_name}-b", "rb") as file_:
+        tensor = np.frombuffer(file_.read(), dtype=np.float32)
+        module.bias.data = torch.tensor(tensor).reshape(out_f)
+
+
+def load_layernorm(module: nn.LayerNorm, layer: int, idx: int) -> None:
+    with open(f"models/124M/raw/model-h{layer}-ln_{idx}-b", "rb") as file_:
+        tensor = np.frombuffer(file_.read(), dtype=np.float32)
+        module.weight.data = torch.tensor(tensor)
+
+    with open(f"models/124M/raw/model-h{layer}-ln_{idx}-g", "rb") as file_:
+        tensor = np.frombuffer(file_.read(), dtype=np.float32)
+        module.bias.data = torch.tensor(tensor)
+
+
+def load_attention(module: CausalSelfAttention, layer: int, n_embd: int) -> int:
+    load_linear(module.c_attn, f"h{layer}-attn-c_attn", n_embd, 3 * n_embd)
+    load_linear(module.c_proj, f"h{layer}-attn-c_proj", n_embd, n_embd)
+
+
+def load_mlp(module: MLP, layer: int, n_embd: int) -> None:
+    load_linear(module.c_fc, f"h{layer}-mlp-c_fc", n_embd, 4 * n_embd)
+    load_linear(module.c_proj, f"h{layer}-mlp-c_proj", 4 * n_embd, n_embd)
+
+
+def load_block(module: Block, layer: int, n_embd: int) -> None:
+    load_layernorm(module.ln_1, layer, 1)
+    load_attention(module.attn, layer, n_embd)
+    load_layernorm(module.ln_2, layer, 2)
+    load_mlp(module.mlp, layer, n_embd)
+
+
+gpt_config = GPTConfig(dropout=0.2)  # Dropout should have no effect for inference.
+with open(f"models/124M/raw/model-wte", "rb") as file_:
     tensor = np.frombuffer(file_.read(), dtype=np.float32)
-    mlp.c_fc.weight.data = torch.tensor(tensor).reshape(4 * n_embd, n_embd)
+    inputs = torch.tensor(tensor)[: 3 * 5 * gpt_config.n_embd].reshape(3, 5, -1)
 
-with open("models/124M/raw/model-h0-mlp-c_fc-b", "rb") as file_:
-    tensor = np.frombuffer(file_.read(), dtype=np.float32)
-    mlp.c_fc.bias.data = torch.tensor(tensor).reshape(4 * n_embd)
-
-with open("models/124M/raw/model-h0-mlp-c_proj-w", "rb") as file_:
-    tensor = np.frombuffer(file_.read(), dtype=np.float32)
-    mlp.c_proj.weight.data = torch.tensor(tensor).reshape(n_embd, 4 * n_embd)
-
-with open("models/124M/raw/model-h0-mlp-c_proj-b", "rb") as file_:
-    tensor = np.frombuffer(file_.read(), dtype=np.float32)
-    mlp.c_proj.bias.data = torch.tensor(tensor).reshape(n_embd)
-
-with open("models/124M/raw/model-wte", "rb") as file_:
-    tensor = np.frombuffer(file_.read(), dtype=np.float32)
-    inputs = torch.tensor(tensor).reshape(-1, 768)[:3]
-
-outputs = mlp(inputs)
+block = Block(gpt_config)
+load_block(block, 0, gpt_config.n_embd)
+block = block.eval()
+outputs = block(inputs)
 
 name_to_tensor = {
     "gpt_inputs": inputs,
--- src/main.zig
+++ src/main.zig
@@ -32,20 +32,135 @@ pub fn MLP(comptime n_embed: usize) type {
     };
 }
 
+pub fn Block(comptime n_heads: usize, comptime seq_len: usize, comptime head_dim: usize) type {
+    const n_embed = n_heads * head_dim;
+    const ln_t = ops.LayerNorm(n_embed);
+    const attn_t = ops.CausalSelfAttention(n_heads, seq_len, head_dim);
+    const mlp_t = MLP(n_embed);
+
+    return struct {
+        const Self = @This();
+        ln_1: ln_t,
+        attn: attn_t,
+        ln_2: ln_t,
+        mlp: mlp_t,
+
+        pub fn init(
+            ln_1_weight: []const f32,
+            ln_1_bias: []const f32,
+            c_attn_weight: []const f32,
+            c_attn_bias: []const f32,
+            c_proj_weight: []const f32,
+            c_proj_bias: []const f32,
+            ln_2_weight: []const f32,
+            ln_2_bias: []const f32,
+            c_fc_weight: []const f32,
+            c_fc_bias: []const f32,
+            mlp_c_proj_weight: []const f32,
+            mlp_c_proj_bias: []const f32,
+        ) Self {
+            return Self{
+                .ln_1 = ln_t.init(ln_1_weight, ln_1_bias),
+                .attn = attn_t.init(c_attn_weight, c_attn_bias, c_proj_weight, c_proj_bias),
+                .ln_2 = ln_t.init(ln_2_weight, ln_2_bias),
+                .mlp = mlp_t.init(c_fc_weight, c_fc_bias, mlp_c_proj_weight, mlp_c_proj_bias),
+            };
+        }
+
+        pub fn forward(self: Self, inputs: []f32, allocator: *const std.mem.Allocator) ![]f32 {
+            // Create a copy of x for residual computation.
+            var x = try allocator.alloc(f32, inputs.len);
+            std.mem.copyForwards(f32, x, inputs);
+
+            self.ln_1.forward(x);
+            x = try self.attn.forward(x, allocator);
+            for (0..x.len) |i| {
+                x[i] += inputs[i];
+                inputs[i] = x[i];
+            }
+            self.ln_2.forward(x);
+            x = try self.mlp.forward(x, allocator);
+            for (0..x.len) |i| {
+                x[i] += inputs[i];
+            }
+            return x;
+        }
+    };
+}
+
 pub fn expectTensorsApproxEqual(expected: []const f32, actual: []const f32) !void {
     for (0..expected.len) |i| {
         try std.testing.expectApproxEqAbs(
             expected[i],
             actual[i],
-            5e-6,
+            5e-4,
         );
     }
 }
 
 pub fn main() !void {
-    const n_embed = 768;
+    const n_heads = 12;
+    const head_dim = 64;
+    const n_embed = n_heads * head_dim;
+    const batch_size = 3;
+    const seq_len = 5;
 
     const allocator = std.heap.page_allocator;
+    const ln_1_weight = try ops.load_tensor(
+        "models/124M/raw/model-h0-ln_1-b",
+        &[_]usize{n_embed},
+        f32,
+        &allocator,
+    );
+    defer allocator.free(ln_1_weight);
+    const ln_1_bias = try ops.load_tensor(
+        "models/124M/raw/model-h0-ln_1-g",
+        &[_]usize{n_embed},
+        f32,
+        &allocator,
+    );
+    defer allocator.free(ln_1_bias);
+    var c_attn_weight = try ops.load_tensor(
+        "models/124M/raw/model-h0-attn-c_attn-w",
+        &[_]usize{ n_embed, 3 * n_embed },
+        f32,
+        &allocator,
+    );
+    defer allocator.free(c_attn_weight);
+    var c_attn_bias = try ops.load_tensor(
+        "models/124M/raw/model-h0-attn-c_attn-b",
+        &[_]usize{3 * n_embed},
+        f32,
+        &allocator,
+    );
+    var c_proj_weight = try ops.load_tensor(
+        "models/124M/raw/model-h0-attn-c_proj-w",
+        &[_]usize{ n_embed, n_embed },
+        f32,
+        &allocator,
+    );
+    defer allocator.free(c_proj_weight);
+    var c_proj_bias = try ops.load_tensor(
+        "models/124M/raw/model-h0-attn-c_proj-b",
+        &[_]usize{n_embed},
+        f32,
+        &allocator,
+    );
+    defer allocator.free(c_proj_bias);
+    const ln_2_weight = try ops.load_tensor(
+        "models/124M/raw/model-h0-ln_2-b",
+        &[_]usize{n_embed},
+        f32,
+        &allocator,
+    );
+    defer allocator.free(ln_2_weight);
+    const ln_2_bias = try ops.load_tensor(
+        "models/124M/raw/model-h0-ln_2-g",
+        &[_]usize{n_embed},
+        f32,
+        &allocator,
+    );
+    defer allocator.free(ln_2_bias);
     const c_fc_weight = try ops.load_tensor(
         "models/124M/raw/model-h0-mlp-c_fc-w",
         &[_]usize{ n_embed, 4 * n_embed },
@@ -60,37 +175,50 @@ pub fn main() !void {
         &allocator,
     );
     defer allocator.free(c_fc_bias);
-    const c_proj_weight = try ops.load_tensor(
+    const mlp_c_proj_weight = try ops.load_tensor(
         "models/124M/raw/model-h0-mlp-c_proj-w",
         &[_]usize{ 4 * n_embed, n_embed },
         f32,
         &allocator,
     );
-    defer allocator.free(c_proj_weight);
-    const c_proj_bias = try ops.load_tensor(
+    defer allocator.free(mlp_c_proj_weight);
+    const mlp_c_proj_bias = try ops.load_tensor(
         "models/124M/raw/model-h0-mlp-c_proj-b",
         &[_]usize{n_embed},
         f32,
         &allocator,
     );
-    defer allocator.free(c_proj_bias);
+    defer allocator.free(mlp_c_proj_bias);
     const inputs = try ops.load_tensor(
         "models/test/gpt_inputs",
-        &[_]usize{ 3, n_embed },
+        &[_]usize{ batch_size, seq_len, n_embed },
         f32,
         &allocator,
     );
     defer allocator.free(inputs);
     const expected = try ops.load_tensor(
         "models/test/gpt_outputs",
-        &[_]usize{ 3, n_embed },
+        &[_]usize{ batch_size, seq_len, n_embed },
         f32,
         &allocator,
     );
     defer allocator.free(expected);
 
-    const mlp = MLP(n_embed).init(c_fc_weight, c_fc_bias, c_proj_weight, c_proj_bias);
-    const actual = try mlp.forward(inputs, &allocator);
+    const block = Block(n_heads, seq_len, head_dim).init(
+        ln_1_weight,
+        ln_1_bias,
+        c_attn_weight,
+        c_attn_bias,
+        c_proj_weight,
+        c_proj_bias,
+        ln_2_weight,
+        ln_2_bias,
+        c_fc_weight,
+        c_fc_bias,
+        mlp_c_proj_weight,
+        mlp_c_proj_bias,
+    );
+    const actual = try block.forward(inputs, &allocator);
     defer allocator.free(actual);
 
     try expectTensorsApproxEqual(expected, actual);
