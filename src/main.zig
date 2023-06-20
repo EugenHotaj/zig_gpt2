90f0ca19c0669213179b84c65724f7d1565992a3
EugenHotaj <eugen_hotaj_91@hotmail.com>
2023-06-19 23:44:24 -0400

    Create some helpers for loading blocks.

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,20 @@
 const std = @import("std");
 const ops = @import("ops.zig");
 
+const GPTConfig = struct {
+    const Self = @This();
+
+    n_heads: usize,
+    seq_len: usize,
+    head_dim: usize,
+    n_embed: usize,
+
+    pub fn init(n_heads: usize, seq_len: usize, head_dim: usize) Self {
+        const n_embed = n_heads * head_dim;
+        return Self{ .n_heads = n_heads, .seq_len = seq_len, .head_dim = head_dim, .n_embed = n_embed };
+    }
+};
+
 const MLP = struct {
     const Self = @This();
 
@@ -60,126 +74,106 @@ pub fn expectTensorsApproxEqual(expected: []const f32, actual: []const f32) !voi
     }
 }
 
+pub fn load_linear(
+    name: []const u8,
+    in_features: usize,
+    out_features: usize,
+    allocator: std.mem.Allocator,
+) !ops.Linear {
+    const weight_path = try std.fmt.allocPrint(allocator, "models/124M/raw/model-{s}-w", .{name});
+    defer allocator.free(weight_path);
+    var weight = try ops.load_tensor(
+        weight_path,
+        &[_]usize{ in_features, out_features },
+        f32,
+        &allocator,
+    );
+    const bias_path = try std.fmt.allocPrint(allocator, "models/124M/raw/model-{s}-b", .{name});
+    defer allocator.free(bias_path);
+    var bias = try ops.load_tensor(
+        bias_path,
+        &[_]usize{out_features},
+        f32,
+        &allocator,
+    );
+    return ops.Linear.init(in_features, out_features, weight, bias);
+}
+
+pub fn load_layer_norm(
+    name: []const u8,
+    n_features: usize,
+    allocator: std.mem.Allocator,
+) !ops.LayerNorm {
+    const weight_path = try std.fmt.allocPrint(allocator, "models/124M/raw/model-{s}-b", .{name});
+    defer allocator.free(weight_path);
+    var weight = try ops.load_tensor(
+        weight_path,
+        &[_]usize{n_features},
+        f32,
+        &allocator,
+    );
+    const bias_path = try std.fmt.allocPrint(allocator, "models/124M/raw/model-{s}-g", .{name});
+    defer allocator.free(bias_path);
+    var bias = try ops.load_tensor(
+        bias_path,
+        &[_]usize{n_features},
+        f32,
+        &allocator,
+    );
+    return ops.LayerNorm.init(n_features, weight, bias);
+}
+
+pub fn load_block(layer_idx: usize, config: GPTConfig, allocator: std.mem.Allocator) !Block {
+    const ln_1_name = try std.fmt.allocPrint(allocator, "h{any}-ln_1", .{layer_idx});
+    defer allocator.free(ln_1_name);
+    const ln_1 = try load_layer_norm(ln_1_name, config.n_embed, allocator);
+
+    const c_attn_name = try std.fmt.allocPrint(allocator, "h{any}-attn-c_attn", .{layer_idx});
+    defer allocator.free(c_attn_name);
+    const c_attn = try load_linear(c_attn_name, config.n_embed, 3 * config.n_embed, allocator);
+
+    const c_proj_name = try std.fmt.allocPrint(allocator, "h{any}-attn-c_proj", .{layer_idx});
+    defer allocator.free(c_proj_name);
+    const c_proj = try load_linear(c_proj_name, config.n_embed, config.n_embed, allocator);
+
+    const ln_2_name = try std.fmt.allocPrint(allocator, "h{any}-ln_2", .{layer_idx});
+    defer allocator.free(ln_2_name);
+    const ln_2 = try load_layer_norm(ln_2_name, config.n_embed, allocator);
+
+    const c_fc_name = try std.fmt.allocPrint(allocator, "h{any}-mlp-c_fc", .{layer_idx});
+    defer allocator.free(c_fc_name);
+    const c_fc = try load_linear(c_fc_name, config.n_embed, 4 * config.n_embed, allocator);
+
+    const mlp_c_proj_name = try std.fmt.allocPrint(allocator, "h{any}-mlp-c_proj", .{layer_idx});
+    defer allocator.free(mlp_c_proj_name);
+    const mlp_c_proj = try load_linear(mlp_c_proj_name, 4 * config.n_embed, config.n_embed, allocator);
+
+    const attn = ops.CausalSelfAttention.init(config.n_heads, config.seq_len, config.head_dim, c_attn, c_proj);
+    const mlp = MLP.init(c_fc, mlp_c_proj);
+    return Block.init(ln_1, attn, ln_2, mlp);
+}
+
 pub fn main() !void {
-    const n_heads = 12;
-    const head_dim = 64;
-    const n_embed = n_heads * head_dim;
     const batch_size = 3;
-    const seq_len = 5;
+    const config = GPTConfig.init(12, 5, 64);
 
     const allocator = std.heap.page_allocator;
-    const ln_1_weight = try ops.load_tensor(
-        "models/124M/raw/model-h0-ln_1-b",
-        &[_]usize{n_embed},
-        f32,
-        &allocator,
-    );
-    defer allocator.free(ln_1_weight);
-    const ln_1_bias = try ops.load_tensor(
-        "models/124M/raw/model-h0-ln_1-g",
-        &[_]usize{n_embed},
-        f32,
-        &allocator,
-    );
-    defer allocator.free(ln_1_bias);
-    var c_attn_weight = try ops.load_tensor(
-        "models/124M/raw/model-h0-attn-c_attn-w",
-        &[_]usize{ n_embed, 3 * n_embed },
-        f32,
-        &allocator,
-    );
-    defer allocator.free(c_attn_weight);
-    var c_attn_bias = try ops.load_tensor(
-        "models/124M/raw/model-h0-attn-c_attn-b",
-        &[_]usize{3 * n_embed},
-        f32,
-        &allocator,
-    );
-    var c_proj_weight = try ops.load_tensor(
-        "models/124M/raw/model-h0-attn-c_proj-w",
-        &[_]usize{ n_embed, n_embed },
-        f32,
-        &allocator,
-    );
-    defer allocator.free(c_proj_weight);
-    var c_proj_bias = try ops.load_tensor(
-        "models/124M/raw/model-h0-attn-c_proj-b",
-        &[_]usize{n_embed},
-        f32,
-        &allocator,
-    );
-    defer allocator.free(c_proj_bias);
-    const ln_2_weight = try ops.load_tensor(
-        "models/124M/raw/model-h0-ln_2-b",
-        &[_]usize{n_embed},
-        f32,
-        &allocator,
-    );
-    defer allocator.free(ln_2_weight);
-    const ln_2_bias = try ops.load_tensor(
-        "models/124M/raw/model-h0-ln_2-g",
-        &[_]usize{n_embed},
-        f32,
-        &allocator,
-    );
-    defer allocator.free(ln_2_bias);
-    const c_fc_weight = try ops.load_tensor(
-        "models/124M/raw/model-h0-mlp-c_fc-w",
-        &[_]usize{ n_embed, 4 * n_embed },
-        f32,
-        &allocator,
-    );
-    defer allocator.free(c_fc_weight);
-    const c_fc_bias = try ops.load_tensor(
-        "models/124M/raw/model-h0-mlp-c_fc-b",
-        &[_]usize{4 * n_embed},
-        f32,
-        &allocator,
-    );
-    defer allocator.free(c_fc_bias);
-    const mlp_c_proj_weight = try ops.load_tensor(
-        "models/124M/raw/model-h0-mlp-c_proj-w",
-        &[_]usize{ 4 * n_embed, n_embed },
-        f32,
-        &allocator,
-    );
-    defer allocator.free(mlp_c_proj_weight);
-    const mlp_c_proj_bias = try ops.load_tensor(
-        "models/124M/raw/model-h0-mlp-c_proj-b",
-        &[_]usize{n_embed},
-        f32,
-        &allocator,
-    );
-    defer allocator.free(mlp_c_proj_bias);
+    const block = try load_block(0, config, allocator);
     const inputs = try ops.load_tensor(
         "models/test/gpt_inputs",
-        &[_]usize{ batch_size, seq_len, n_embed },
+        &[_]usize{ batch_size, config.seq_len, config.n_embed },
         f32,
         &allocator,
     );
     defer allocator.free(inputs);
     const expected = try ops.load_tensor(
         "models/test/gpt_outputs",
-        &[_]usize{ batch_size, seq_len, n_embed },
+        &[_]usize{ batch_size, config.seq_len, config.n_embed },
         f32,
         &allocator,
     );
     defer allocator.free(expected);
 
-    const ln_1 = ops.LayerNorm.init(n_embed, ln_1_weight, ln_1_bias);
-    const ln_2 = ops.LayerNorm.init(n_embed, ln_2_weight, ln_2_bias);
-    const attn = ops.CausalSelfAttention.init(
-        n_heads,
-        seq_len,
-        head_dim,
-        ops.Linear.init(n_embed, 3 * n_embed, c_attn_weight, c_attn_bias),
-        ops.Linear.init(n_embed, n_embed, c_proj_weight, c_proj_bias),
-    );
-    const mlp = MLP.init(
-        ops.Linear.init(n_embed, 4 * n_embed, c_fc_weight, c_fc_bias),
-        ops.Linear.init(4 * n_embed, n_embed, mlp_c_proj_weight, mlp_c_proj_bias),
-    );
-    const block = Block.init(ln_1, attn, ln_2, mlp);
     const actual = try block.forward(inputs, &allocator);
     defer allocator.free(actual);
 
